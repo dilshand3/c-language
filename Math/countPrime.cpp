54443662f13513f9a2a55af34f2b5a6775021cc4
dilshan.d3 <dilshan72404@gmail.com>
2025-11-21 09:11:42 +0530

    add initial implementation of prime number checking and counting

---
+++ Math/countPrime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n value: ";
+    cin >> n;
+    vector<bool> vec(n+1,true);
+    int count = 0;
+    for(int i = 2;i<n;i++){
+        
+    }
+    return 0;
+}
